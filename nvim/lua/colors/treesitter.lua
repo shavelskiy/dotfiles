84f7e38d76a7db3492aced26d03aff90ac1f7b6d
Vladislav Shavelskiy <v.shavelskiy@192.168.1.4>
2023-01-09 01:50:22 +0300

    update config

--- nvim/lua/colors/treesitter.lua
+++ nvim/lua/colors/treesitter.lua
@@ -92,7 +92,7 @@ return {
   { '@variable.php', { fg = colors.base0C } },
   { '@variable.builtin.php', { fg = colors.base0C } },
   { '@variable.php', { fg = colors.base0C } },
-  -- { '@function.php', { fg = colors.base0B } },
+  { '@function.php', { fg = colors.base0B } },
   { '@const.builtin.php', { fg = colors.base0E } },
   { '@exception.php', { fg = colors.base0E, italic = true } },
   { '@error.php', { fg = colors.base05 } },
