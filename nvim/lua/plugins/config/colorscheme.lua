01a96b88720f4bd6d96f8d43a941954169c46379
Vladislav Shavelskiy <v.shavelskiy@192.168.1.4>
2023-01-11 10:48:02 +0300

    add iterm2 themes

--- nvim/lua/plugins/config/colorscheme.lua
+++ nvim/lua/plugins/config/colorscheme.lua
@@ -1,14 +1,9 @@
-local theme = 'atelier-savanna'
 -- local theme = 'ayu-dark'
 -- local theme = 'ayu-mirage'
--- local theme = 'black-metal-immortal'
 -- local theme = 'catppuccin'
--- local theme = 'da-one-ocean'
 -- local theme = 'default'
 -- local theme = 'gruvbox'
--- local theme = 'rose-pine-moon'
--- local theme = 'rose-pine'
--- local theme = 'tokyodark-terminal'
+local theme = 'tokyodark'
 
 require('colorscheme').setup {
   theme = theme,
