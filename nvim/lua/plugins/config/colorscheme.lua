179caf2b8350910692d56df421d054c83eaa7de4
Vladislav Shavelskiy <v.shavelskiy@192.168.1.4>
2023-01-11 17:05:26 +0300

    default colorscheme

--- nvim/lua/plugins/config/colorscheme.lua
+++ nvim/lua/plugins/config/colorscheme.lua
@@ -1,8 +1,8 @@
 -- local theme = 'ayu-dark'
 -- local theme = 'ayu-mirage'
 -- local theme = 'catppuccin'
--- local theme = 'default'
-local theme = 'gruvbox'
+local theme = 'default'
+-- local theme = 'gruvbox'
 -- local theme = 'tokyodark'
 
 require('colorscheme').setup {
