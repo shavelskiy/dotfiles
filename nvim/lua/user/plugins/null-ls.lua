06476cd77974cf8fec5148217d951d4290826a7e
Vladislav Shavelskiy <v.shavelskiy@192.168.1.3>
2023-02-16 14:33:09 +0300

    update

--- nvim/lua/user/plugins/null-ls.lua
+++ nvim/lua/user/plugins/null-ls.lua
@@ -2,10 +2,9 @@ local null_ls = require 'null-ls'
 
 null_ls.setup {
   sources = {
-    null_ls.builtins.formatting.prettier.with { filetypes = { 'xml', 'yaml', 'twig', 'json', 'markdown' } },
+    null_ls.builtins.formatting.prettier.with { filetypes = { 'xml', 'html', 'yaml', 'twig', 'json', 'markdown' } },
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.twigcs,
-    null_ls.builtins.diagnostics.tidy,
     null_ls.builtins.diagnostics.jsonlint,
   },
 }
