dd8166612d2d8a2daa8925b059ef28b5f6415bb9
Vladislav Shavelskiy <v.shavelskiy@192.168.1.8>
2023-02-21 19:10:11 +0300

    update colors

--- nvim/lua/colorscheme/init.lua
+++ nvim/lua/colorscheme/init.lua
@@ -1,11 +1,7 @@
 if vim.fn.exists 'syntax_on' then vim.cmd 'syntax reset' end
 
 -- local theme = 'ayu-dark'
--- local theme = 'ayu-mirage'
--- local theme = 'catppuccin'
--- local theme = 'default'
 -- local theme = 'gruvbox'
--- local theme = 'tokyodark'
 local theme = 'nightfly'
 
 require('colorscheme.scheme').set_theme(theme)
--- nvim/lua/colorscheme/themes/override/init.lua
+++ nvim/lua/colorscheme/themes/override/init.lua
@@ -1,14 +1,41 @@
 local colors = require('colorscheme.scheme').get_colors()
 
 return {
+  { 'PMenuSel', { fg = colors.base01, bg = colors.base0D } },
+
   { '@field.yaml', { fg = colors.base08 } },
   { '@constant.bash', { fg = colors.base08 } },
   { '@label.json', { fg = colors.base08 } },
 
-  { 'PMenuSel', { fg = colors.base01, bg = colors.base0D } },
+  { '@punctuation.delimiter', { fg = colors.base0C } },
 
   { '@tag.twig', { fg = colors.base0E } },
 
   { '@tag.html', { fg = colors.base0D } },
   { '@tag.delimiter.html', { fg = colors.base0D } },
+
+  -- php
+  { '@string.php', { fg = colors.base0A } },
+  { '@property.php', { fg = colors.base07 } },
+  { '@variable.php', { fg = colors.base0C } },
+  { '@function.php', { fg = colors.base0B } },
+  { '@type.php', { fg = colors.base07 } },
+  { '@constructor.php', { fg = colors.base07 } },
+  { '@constant.php', { fg = colors.base08 } },
+  { '@constant.builtin.php', { fg = colors.base08 } },
+  { '@number.php', { fg = colors.base08 } },
+  { '@boolean.php', { fg = colors.base08 } },
+  { '@type.builtin.php', { fg = colors.base0E } },
+  { '@include.php', { fg = colors.base0E } },
+  { '@namespace.php', { fg = colors.base07 } },
+  { '@function.call.php', { fg = colors.base0B } },
+  { '@variable.builtin.php', { fg = colors.base0C } },
+  { '@exception.php', { fg = colors.base0E } },
+  { '@operator.php', { fg = colors.base0C } },
+  { '@punctuation.delimiter.php', { fg = colors.base0C } },
+  { '@punctuation.bracket.php', { fg = colors.base0C } },
+  { '@error.php', { fg = colors.base05 } },
+  { '@parameter.php', { fg = colors.base0D } },
+  { '@attribute.php', { fg = colors.base0D } },
+  { '@type.phpdoc', { fg = colors.base07 } },
 }
