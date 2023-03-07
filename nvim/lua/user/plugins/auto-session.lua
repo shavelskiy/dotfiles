ba3e93a864a699ad02d492a0fd7b673a36ff6c46
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-03-07 22:12:59 +0300

    update auto session

--- nvim/lua/user/plugins/auto-session.lua
+++ nvim/lua/user/plugins/auto-session.lua
@@ -1,4 +1,19 @@
 return {
   'rmagatti/auto-session',
-  config = function() require('auto-session').setup {} end,
+  config = function()
+    local function close_nvim_tree() require('nvim-tree.view').close() end
+    local function open_nvim_tree() require('nvim-tree').open() end
+
+    require('auto-session').setup {
+      pre_save_cmds = { close_nvim_tree },
+      post_save_cmds = { open_nvim_tree },
+      post_open_cmds = { open_nvim_tree },
+      post_restore_cmds = { open_nvim_tree },
+      cwd_change_handling = {
+        restore_upcoming_session = true,
+        pre_cwd_changed_hook = close_nvim_tree,
+        post_cwd_changed_hook = open_nvim_tree,
+      },
+    }
+  end,
 }
