2e5a8f1b3a14942d34c97ed1da8d8fbd54ff2d66
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2023-10-23 23:48:28 +0300

    update indentline

--- nvim/lua/colorscheme/modules/indentline.lua
+++ nvim/lua/colorscheme/modules/indentline.lua
@@ -1,6 +1,7 @@
 local colors = require 'colorscheme.colors'
 
 return {
+  { '@ibl', { fg = colors.base0E } },
   { '@ibl.indent.char.1', { fg = colors.base02 } },
   { '@ibl.whitespace.char.1', { fg = 'none' } },
 }
--- nvim/lua/user/plugins/indent-blankline.lua
+++ nvim/lua/user/plugins/indent-blankline.lua
@@ -6,10 +6,6 @@ return {
     }
     require('ibl').setup {
       indent = { highlight = highlight, char = '│' },
-      whitespace = {
-        highlight = highlight,
-        remove_blankline_trail = false,
-      },
       scope = { enabled = false },
     }
   end,
