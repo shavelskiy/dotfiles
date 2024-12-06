8706dbe6c73c4498d55932c4e4578e03a79b7bbd
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2024-12-06 14:17:41 +0300

    delete neoscroll

--- nvim/lua/user/plugins/neoscroll.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'karb94/neoscroll.nvim',
-  keys = {
-    { '<C-u>', function() require('neoscroll').ctrl_u { duration = 100 } end },
-    { '<C-d>', function() require('neoscroll').ctrl_d { duration = 100 } end },
-    { '<C-b>', function() require('neoscroll').ctrl_b { duration = 100 } end },
-    { '<C-f>', function() require('neoscroll').ctrl_f { duration = 100 } end },
-  },
-}
--- nvim/lua/user/plugins/telescope.lua
+++ nvim/lua/user/plugins/telescope.lua
@@ -2,7 +2,7 @@ local execute = function(exec)
   if vim.bo[vim.api.nvim_win_get_buf(0)].ft ~= 'NvimTree' then
     exec()
   else
-    local node = require('nvim-tree.lib').get_node_at_cursor()
+    local node = require('nvim-tree.core').get_explorer():get_node_at_cursor()
     if not node then return end
     exec { search_dirs = { node.open == nil and node.parent.absolute_path or node.absolute_path } }
   end
