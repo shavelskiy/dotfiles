2cd83679a9527a41f40da8cf7754eef4a6320fb2
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2025-05-27 01:12:50 +0300

    update

--- nvim/lua/user/plugins/mason.lua
+++ nvim/lua/user/plugins/mason.lua
@@ -1,44 +1,55 @@
 return {
-  'williamboman/mason.nvim',
-  dependencies = {
-    'williamboman/mason-lspconfig.nvim',
-    'jay-babu/mason-null-ls.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
+  {
+    'mason-org/mason.nvim',
+    config = function()
+      require('mason').setup {
+        ui = {
+          height = 0.8,
+          icons = {
+            package_pending = ' ',
+            package_installed = ' ',
+            package_uninstalled = ' ﮊ',
+          },
+          keymaps = {
+            toggle_server_expand = '<CR>',
+            install_server = 'i',
+            update_server = 'u',
+            check_server_version = 'c',
+            update_all_servers = 'U',
+            check_outdated_servers = 'C',
+            uninstall_server = 'X',
+            cancel_installation = '<C-c>',
+          },
+        },
+
+        max_concurrent_installers = 10,
+      }
+    end,
   },
-  config = function()
-    require('mason').setup {
-      ui = {
-        height = 0.8,
-        icons = {
-          package_pending = ' ',
-          package_installed = ' ',
-          package_uninstalled = ' ﮊ',
-        },
-        keymaps = {
-          toggle_server_expand = '<CR>',
-          install_server = 'i',
-          update_server = 'u',
-          check_server_version = 'c',
-          update_all_servers = 'U',
-          check_outdated_servers = 'C',
-          uninstall_server = 'X',
-          cancel_installation = '<C-c>',
-        },
-      },
-
-      max_concurrent_installers = 10,
-    }
-
-    require('mason-lspconfig').setup {
-      ensure_installed = require 'user.servers'
-    }
-
-    require('mason-null-ls').setup {
-      ensure_installed = { 'prettier', 'stylua', 'twigcs', 'sqlfluff' },
-    }
-
-    require('mason-nvim-dap').setup {
-      ensure_installed = { 'php', 'delve' },
-    }
-  end,
+  {
+    'mason-org/mason-lspconfig.nvim',
+    commit = '1a31f82',
+    dependencies = { 'neovim/nvim-lspconfig' },
+    config = function()
+      require('mason-lspconfig').setup {
+        ensure_installed = require 'user.servers',
+      }
+    end,
+  },
+  {
+    'jay-babu/mason-null-ls.nvim',
+    config = function()
+      require('mason-null-ls').setup {
+        ensure_installed = { 'prettier', 'cspell', 'stylua', 'twigcs', 'sqlfluff' },
+      }
+    end,
+  },
+  {
+    'jay-babu/mason-nvim-dap.nvim',
+    config = function()
+      require('mason-nvim-dap').setup {
+        ensure_installed = { 'php', 'delve' },
+      }
+    end,
+  },
 }
