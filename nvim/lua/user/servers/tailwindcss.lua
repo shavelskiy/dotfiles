36fd38918a2f51b805ad11eafce299a13f49f92a
Vladislav Shavelskiy <v.shavelsky@gmail.com>
2025-05-27 23:21:56 +0300

    work

--- nvim/lua/user/servers/tailwindcss.lua
+++ nvim/lua/user/servers/tailwindcss.lua
@@ -1,7 +1,7 @@
 return {
   root_dir = function(fname)
     local root_pattern =
-      require('lspconfig').util.root_pattern('tailwind.config.cjs', 'tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js', 'postcss.config.mjs')
+      require('lspconfig').util.root_pattern('tailwind.config.cjs', 'tailwind.config.js', 'tailwind.config.ts', 'postcss.config.js', 'postcss.config.mjs', 'vite.config.ts')
     return root_pattern(fname)
   end,
 }
